0bf05c80c915ac892a857250cc87235f28f658a3
Duong Tung Anh <tunnaduong@gmail.com>
2022-03-17 15:57:31 +0700

    changed index.html to index.php

--- index.php
+++ index.php
@@ -1,51 +1,48 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head id="include-head">
+
+<head id="include-head">
     <meta charset="UTF-8" />
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-  </head>
-  <body>
+</head>
+
+<body>
     <div id="root">
-      <div class="screen">
-        <div id="include-menu"></div>
-        <div id="include-content"></div>
-      </div>
+        <div class="screen">
+            <div id="include-menu"></div>
+            <div id="include-content"></div>
+        </div>
     </div>
-    <script
-      type="module"
-      src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"
-    ></script>
-    <script
-      nomodule
-      src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"
-    ></script>
+    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
     <script>
-      $("#include-head").load("/includes/head.html");
-      $("#include-menu").load("/includes/menu.html");
-      if (
+    $("#include-head").load("/includes/head.html");
+    $("#include-menu").load("/includes/menu.html");
+    if (
         window.location.pathname.split("/")[1] === "" ||
-        window.location.pathname.split("/")[1] === "index.html"
-      ) {
+        window.location.pathname.split("/")[1] === "index.php"
+    ) {
         $("#include-content").load("/pages/home.html");
-        $(window).on("load", function () {
-          $("#page-title").text("JPro - A project of JSClub Team 1");
-          $("#home-btn").addClass("nav-btn-active");
+        $(window).on("load", function() {
+            $("#page-title").text("JPro - A project of JSClub Team 1");
+            $("#home-btn").addClass("nav-btn-active");
         });
-      } else if (
+    } else if (
         window.location.pathname.split("/")[1] === "discussions" &&
         window.location.pathname.split("/")[2] &&
         window.location.pathname.split("/")[3] === "detail"
-      ) {
+    ) {
         $("#include-content").load("/pages/discussion_detail.html");
-        $(window).on("load", function () {
-          $("#page-title").text("Cuộc thảo luận - JPro");
+        $(window).on("load", function() {
+            $("#page-title").text("Cuộc thảo luận - JPro");
         });
-      } else {
+    } else {
         $("#include-content").load("/pages/errors/404.html");
-        $(window).on("load", function () {
-          $("#page-title").text("Oops, không tìm thấy nội dung - JPro");
+        $(window).on("load", function() {
+            $("#page-title").text("Oops, không tìm thấy nội dung - JPro");
         });
-      }
+    }
     </script>
-  </body>
+</body>
+
 </html>
