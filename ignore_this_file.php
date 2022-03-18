4211c2cef8a4a964cb550ab9ec3050db859e46c8
Duong Tung Anh <tunnaduong@gmail.com>
2022-03-18 11:54:06 +0700

    changed index.php to .html

---
+++ ignore_this_file.php
@@ -0,0 +1,2 @@
+<?php
+echo "This file is created just to fools Heroku PHP Buildpack that our project is made with PHP. If this file didn't exists, Heroku won't let our deploy-fronted to be built. So, don't delete this file please!";
