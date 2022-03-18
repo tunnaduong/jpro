dd83c5ae8a74839d12da1771e887a613aa43cbf8
Duong Tung Anh <tunnaduong@gmail.com>
2022-03-18 12:59:09 +0700

    added composer for heroku deployment

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'cf8570133e3f4a0fd1c9ab908794c134ccb335dc',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'cf8570133e3f4a0fd1c9ab908794c134ccb335dc',
+            'dev_requirement' => false,
+        ),
+    ),
+);
