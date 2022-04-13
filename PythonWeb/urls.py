3e84f73ee85623a1e2ed467e6651f3e917fdb3f5
Duong Tung Anh <tunnaduong@gmail.com>
2022-04-13 18:52:50 +0700

    ok

--- PythonWeb/urls.py
+++ PythonWeb/urls.py
@@ -41,7 +41,7 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/user/', include('users.urls')),
-
+    path('api/', include('blog.urls')),
 ]
 
 if settings.DEBUG:
