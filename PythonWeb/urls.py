5439054e00315f5e8ef87e00a2c63b1006f1fca8
Duong Tung Anh <tunnaduong@gmail.com>
2022-04-13 21:30:01 +0700

    ok

--- PythonWeb/urls.py
+++ PythonWeb/urls.py
@@ -42,10 +42,6 @@ urlpatterns = [
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/user/', include('users.urls')),
     path('api/', include('blog.urls')),
+    static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
+    static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL,
-                          document_root=settings.STATIC_ROOT)
