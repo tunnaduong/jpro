55fa76a005467ac8a2adbcaa79f6a03815a294a8
Duong Tung Anh <tunnaduong@gmail.com>
2022-04-13 13:52:54 +0700

    ok

--- PythonWeb/settings.py
+++ PythonWeb/settings.py
@@ -124,7 +124,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+# PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+# STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
@@ -140,9 +142,9 @@ LOGIN_REDIRECT_URL = 'blog-home'
 LOGIN_URL = 'login'
 
 
-REST_FRAMEWORK = {    
-    'DEFAULT_AUTHENTICATION_CLASSES': (       
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
         'knox.auth.TokenAuthentication',
-    )   
+    )
 }
