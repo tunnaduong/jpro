1e1ec3e24e923d57312e9a4a2c0ed995c507f8af
Duong Tung Anh <tunnaduong@gmail.com>
2022-04-13 15:19:30 +0700

    test

--- PythonWeb/settings.py
+++ PythonWeb/settings.py
@@ -127,7 +127,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 # PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 # STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 # Default primary key field type
