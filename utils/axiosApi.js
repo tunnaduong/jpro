82afd4f657e68a1cfb1bd3c11ebd73b57f260bd9
Duong Tung Anh <tunnaduong@gmail.com>
2022-04-14 03:43:09 +0700

    ok

--- utils/axiosApi.js
+++ utils/axiosApi.js
@@ -1,17 +1,14 @@
-import "../static/js/axios/axios.min.js";
-
 const baseURL = `https://jpro-api.herokuapp.com/`;
 console.log(baseURL);
 const axiosInstance = axios.create({
   baseURL: baseURL,
   timeout: 5000,
   headers: {
-    Authorization: sessionStorage.getItem("access_token")
-      ? "Bearer " + sessionStorage.getItem("access_token")
+    Authorization: localStorage.getItem("access_token")
+      ? "Bearer " + localStorage.getItem("access_token")
       : null,
     "Content-Type": "application/json",
     accept: "application/json",
-    headers: { "Access-Control-Allow-Origin": "*" },
   },
 });
 
@@ -20,7 +17,7 @@ axiosInstance.interceptors.response.use(
   (error) => {
     // Prevent infinite loops
     if (error.response.status === 401) {
-      window.location.href = "/logout";
+      // window.location.href = "/logout";
       return Promise.reject(error);
     }
 
@@ -28,5 +25,3 @@ axiosInstance.interceptors.response.use(
     return Promise.reject(error);
   }
 );
-
-export default axiosInstance;
