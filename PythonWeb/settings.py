bdcf1d2ce0701762958c9bb50e4a25b6f48af390
Duong Tung Anh <tunnaduong@gmail.com>
2022-04-13 14:56:49 +0700

    test

--- PythonWeb/settings.py
+++ PythonWeb/settings.py
@@ -133,7 +133,7 @@ STATICFILES_DIRS = [
 # PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = 'static/'
 # STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
-# STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
